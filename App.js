0cf76e03affdd71825e492733d9d4d748bb47ad3
porkodi25 <porkodins2003@gmail.com>
2024-12-18 15:58:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import CurrentTime from "./component/current-time";
+
+function App() {
+
+  return (
+  
+    <div className="digital-clock">
+      <h1>digital clock</h1>
+      <CurrentTime />
+      
+    </div>
+    
+  
+  );
+}
+
+export default App;
